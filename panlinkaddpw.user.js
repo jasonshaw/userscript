361ec1596b2ad7204a6d020c2a495f054f68712d
jasonshaw <star_xb@163.com>
2014-12-16 11:31:52 +0800

    Update panlinkaddpw.user.js
    更新0.5版：强烈建议更新
    // @note         改变程序逻辑对更多的情况进行支持
    // @note         增加处理盘密码就在链接的文本本身上，比如http://www.tt7z.com/html/EdrawSoft-Edraw-Max.html
    // @note         增加脚本运行判断，解决个别页面动态加载问题，比如新浪微博
    // @note         增加百度贴吧跳转和新浪微博短网址的支持，两条配置一样存在“误杀”，不常用的可以注释掉这两条规则
    // @note         优化代码，将任意单条正则变为可选配置，一般由默认正则处理，有利于自定义扩展和维护

--- panlinkaddpw.user.js
+++ panlinkaddpw.user.js
@@ -1,11 +1,16 @@
 // ==UserScript==
 // @name  配合网盘密码自动提取，融合链接与提取码
 // @namespace  panlink.jasonshaw
-// @version    0.4
+// @version    0.5
 // @description  自动处理网盘链接及其提取码变成支持自动填充密码的方式的链接（百度云、360pan等）
 // @include      *
 // @downloadURL    https://raw.githubusercontent.com/jasonshaw/userscript/master/panlinkaddpw.user.js
 // @updateURL      https://raw.githubusercontent.com/jasonshaw/userscript/master/panlinkaddpw.user.js
+// @note         改变程序逻辑对更多的情况进行支持
+// @note         增加处理盘密码就在链接的文本本身上
+// @note         增加脚本运行判断，解决个别页面动态加载问题，比如新浪微博
+// @note         增加百度贴吧跳转和新浪微博短网址的支持，两条配置一样存在“误杀”，不常用的可以注释掉这两条规则
+// @note         优化代码，将任意单条正则变为可选配置，一般由默认正则处理，有利于自定义扩展和维护
 // @note         增加支持自动更新
 // @note         修正配置，支持单页面多云盘链接
 // @note         避免重复后缀密码
@@ -16,29 +21,39 @@
 // @copyright  2014+, jasonshaw
 // ==/UserScript==
 (function(){
-	function panlinkWithPw(){
+	function panlinkWithPw(){	
+		var common_reg = /\s*(提取密碼|提取密码|提取码|提取碼|提取|密碼|密码|百度|百度云|云盘|360云盘|360云|360yun|yun)[:：]?\s*(<[^>]+>)?\s*([0-9a-zA-Z]{4,})\s*/;
 		var prefs = {
-				tieba:['http://jump.bdimg.com/safecheck',/\s*(提取密碼|提取密码|提取码|提取碼|提取|密碼|密码)[:：]?\s*(<[^>]+>)?\s*([0-9a-zA-Z]{4,})\s*/],
-				pan:['http://pan.baidu.com/s/',/\s*(提取密碼|提取密码|提取码|提取碼|提取|密碼|密码)[:：]?\s*(<[^>]+>)?\s*([0-9a-zA-Z]{4,})\s*/],//第一个参数定义链接类型，后续紧跟着的提取码之类的前缀提示符
-		        yunpan:['http://yunpan.cn/',/\s*(提取密碼|提取密码|提取码|提取碼|提取|密碼|密码)[:：]?\s*(<[^>]+>)?\s*([0-9a-zA-Z]{4,})\s*/],
-		        pans:['https://pan.baidu.com/s/',/\s*(提取密碼|提取密码|提取码|提取碼|提取|密碼|密码)[:：]?\s*(<[^>]+>)?\s*([0-9a-zA-Z]{4,})\s*/],
+				tieba:['http://jump.bdimg.com/safecheck'],//这个有大量的误操作，因为这只是新浪的短网址，而不一定是网盘，自选使用
+				pan:['http://pan.baidu.com/s/'],//第一个参数定义链接类型，第二个可选参数：后续紧跟着的提取码之类的前缀提示符
+		        yunpan:['http://yunpan.cn/'],
+		        pans:['https://pan.baidu.com/s/'],
+		        tpan:['http://t.cn/'],//这个有大量的误操作，因为这只是新浪的短网址，而不一定是网盘，自选使用
 		};
-		var panlinks,r = null,i,nC,nN,pN,pos,subS;
+		var panlinks,r = null,reg,i,nC,nN,pN,pos,subS;
 		for (var key in prefs) {
+			reg = prefs[key][1] || common_reg;
 			panlinks = document.querySelectorAll('a[href^="'+prefs[key][0]+'"]'),i=0;
 			while(panlinks[i]){
 				if(/https?:\/\/pan\.baidu\.com\/s\/\w+#\w{4,}/.test(panlinks[i].href)) {i++;continue;}
 				nN = panlinks[i].nextSibling;
-				if(nN.nodeType===1)nC=nN.innerHTML;
-				else if(nN.nodeType===3) nC=document.all?nN.innerText:nN.textContent;
-				r = nC.match(prefs[key][1]);
-				if(r!=null) panlinks[i].href += '#'+r[3];
-				else {
-					pN = panlinks[i].parentNode.parentNode.innerHTML;
-					pos = pN.indexOf(panlinks[i].href);
-					subS = pN.substr(pN.indexOf(panlinks[i].href));
-					r = subS.match(prefs[key][1]);
+				if(nN!=null) {
+					if(nN.nodeType===1)nC=nN.innerHTML;
+					else if(nN.nodeType===3) nC=document.all?nN.innerText:nN.textContent;
+					r = nC.match(reg);
 					if(r!=null) panlinks[i].href += '#'+r[3];
+				}
+				if(nN==null||r==null) {
+					//处理盘密码就在链接的文本本身上
+					r = panlinks[i].innerHTML.match(reg);
+					if(r!=null) panlinks[i].href += '#'+r[3];
+					else {
+						pN = panlinks[i].parentNode.parentNode.innerHTML;
+						pos = pN.indexOf(panlinks[i].href);
+						subS = pN.substr(pN.indexOf(panlinks[i].href));
+						r = subS.match(reg);
+						if(r!=null) panlinks[i].href += '#'+r[3];
+					}
 				}
 				i++;
 			}
@@ -55,11 +70,10 @@
 		});
 		observer.observe(watch, {childList: true, subtree: true });
 	}
-	panlinkWithPw();
 	// 添加下一页和不刷新页面的支持
 	addMutationObserver('#ct', function(){
-		// console.log('元素被添加')
-		//alert(1);
 		panlinkWithPw();
 	});
+	document.onreadystatechange = function(){if(document.readyState == "complete") panlinkWithPw();}
 })();
+
