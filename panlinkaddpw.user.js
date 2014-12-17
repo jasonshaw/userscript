616554d1d2d4917d815025237a15862d7826c550
jasonshaw <star_xb@163.com>
2014-12-17 20:19:36 +0800

    Update panlinkaddpw.user.js
    // @note         改变程序逻辑以适应一些特殊环境
    // @note         修复一个bug，彻底解决重复后缀密码问题
    // @note         增加脚本运行判断的设定，默认自动运行，对于类似新浪微博类网址，才执行判断后执行

--- panlinkaddpw.user.js
+++ panlinkaddpw.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name  配合网盘密码自动提取，融合链接与提取码
 // @namespace  panlink.jasonshaw
-// @version    0.5
+// @version    0.6
 // @description  自动处理网盘链接及其提取码变成支持自动填充密码的方式的链接（百度云、360pan等）
 // @include      *
 // @downloadURL    https://raw.githubusercontent.com/jasonshaw/userscript/master/panlinkaddpw.user.js
 // @updateURL      https://raw.githubusercontent.com/jasonshaw/userscript/master/panlinkaddpw.user.js
-// @note         遇到无法自动更新的可以将脚本中 @updateURL  改为 // @require 即可
+// @require      https://raw.githubusercontent.com/jasonshaw/userscript/master/panlinkaddpw.user.js
+// @note         改变程序逻辑以适应一些特殊环境
+// @note         修复一个bug，彻底解决重复后缀密码问题
+// @note         增加脚本运行判断的设定，默认自动运行，对于类似新浪微博类网址，才执行判断后执行  
 // @note         改变程序逻辑对更多的情况进行支持
 // @note         增加处理盘密码就在链接的文本本身上
 // @note         增加脚本运行判断，解决个别页面动态加载问题，比如新浪微博
@@ -22,8 +25,12 @@
 // @copyright  2014+, jasonshaw
 // ==/UserScript==
 (function(){
+	//alert(document.querySelectorAll('a[href^="http://pan.baidu.com/s/"]').length);
+	//var delay = 200;//ms
 	function panlinkWithPw(){	
 		var common_reg = /\s*(提取密碼|提取密码|提取码|提取碼|提取|密碼|密码|百度|百度云|云盘|360云盘|360云|360yun|yun)[:：]?\s*(<[^>]+>)?\s*([0-9a-zA-Z]{4,})\s*/;
+		var pw_reg = /#[0-9a-zA-Z]{4,}/;
+		var standBy = false,standByList = [/http:\/\/weibo\.com\/.*/i];//将需要等待js运行之后再运行本代码的，将href正则写入数组
 		var prefs = {
 				tieba:['http://jump.bdimg.com/safecheck'],//这个有大量的误操作，因为这只是新浪的短网址，而不一定是网盘，自选使用
 				pan:['http://pan.baidu.com/s/'],//第一个参数定义链接类型，第二个可选参数：后续紧跟着的提取码之类的前缀提示符
@@ -31,12 +38,14 @@
 		        pans:['https://pan.baidu.com/s/'],
 		        tpan:['http://t.cn/'],//这个有大量的误操作，因为这只是新浪的短网址，而不一定是网盘，自选使用
 		};
+		var href = window.location.href,site = null,i = 0;
+		while (standByList[i]) if(standByList[i++].test(href)) {standBy = true; break;}
 		var panlinks,r = null,reg,i,nC,nN,pN,pos,subS;
 		for (var key in prefs) {
 			reg = prefs[key][1] || common_reg;
 			panlinks = document.querySelectorAll('a[href^="'+prefs[key][0]+'"]'),i=0;
 			while(panlinks[i]){
-				if(/https?:\/\/pan\.baidu\.com\/s\/\w+#\w{4,}/.test(panlinks[i].href)) {i++;continue;}
+				if(pw_reg.test(panlinks[i].href)) {i++;continue;}
 				nN = panlinks[i].nextSibling;
 				if(nN!=null) {
 					if(nN.nodeType===1)nC=nN.innerHTML;
@@ -50,8 +59,12 @@
 					if(r!=null) panlinks[i].href += '#'+r[3];
 					else {
 						pN = panlinks[i].parentNode.parentNode.innerHTML;
+						//截取子串避免密码“张冠李戴”
 						pos = pN.indexOf(panlinks[i].href);
 						subS = pN.substr(pN.indexOf(panlinks[i].href));
+						var pos_end = subS.length,temp;
+						for (var key1 in prefs) { temp = pN.indexOf(prefs[key1][0]);if(temp<pos_end) pos_end = temp}
+						subS = pN.substr(0,pos_end-1);
 						r = subS.match(reg);
 						if(r!=null) panlinks[i].href += '#'+r[3];
 					}
@@ -66,6 +79,7 @@
 		var observer = new MutationObserver(function(mutations){
 			var nodeAdded = mutations.some(function(x){ return x.addedNodes.length > 0; });
 			if (nodeAdded) {
+			// observer.disconnect();
 			callback();
 			}
 		});
@@ -75,6 +89,9 @@
 	addMutationObserver('#ct', function(){
 		panlinkWithPw();
 	});
-	document.onreadystatechange = function(){if(document.readyState == "complete") panlinkWithPw();}
+	if(standBy) {document.onreadystatechange = function () { if(document.readyState == "complete") panlinkWithPw(); }}
+	else panlinkWithPw();
 })();
+
+
 
