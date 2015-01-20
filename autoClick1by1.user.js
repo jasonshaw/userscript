25711568e9fcfdc7c0c396e1dcc139176fc67959
jasonshaw <star_xb@163.com>
2015-01-20 15:17:32 +0800

    Update autoClick1by1.user.js
    更新0.5版：
    // @note         允许在配置中直接给出dom元素 而替代css3 selector数组，参数直接为函数，返回值即dom元素数组
    // @note         不需要的坛友，可以将配置中，rutracker 段 删除，并将include段删除
    // @note         如果没有需要批量选择自动点击元素（即单纯依靠css3 selector 指定一系列困难时，建议更新，否则可以不必更新到0.5版

--- autoClick1by1.user.js
+++ autoClick1by1.user.js
@@ -1,14 +1,19 @@
-// ==UserScript==
 // @name  网页自动化系列点击
 // @namespace  autoClick1by1.jasonshaw
-// @version    0.4
+// @version    0.5
 // @description  匹配的任意url，顺序逐个点击设定的obj，任意不存在则彻底停止
+// @include      [url]http://rutracker.org/forum/viewtopic.php?t=[/url]*
 // @include      http://*.kdslife.com/show/photo/*.html
-// @include      http://www.repaik.com/forum.php?mod=viewthread*
+// @include      [url]http://www.repaik.com/forum.php?mod=viewthread[/url]*
+// @include      [url]http://bbs.kafan.cn/[/url]
+// @include      [url]http://bbs.kafan.cn/forum.php?mod=viewthread[/url]*
+// @include      [url]http://www.repaik.com/[/url]
+// @include      [url]http://www.repaik.com/plugin.php?id=dsu_paulsign:sign[/url]*
+// @include      [url]http://bbs.kafan.cn/thread-[/url]*-*-*.html
 // @include      http://*.pcauto.com.cn/*/*/*/*.html
-// @include      http://bbs.pinggu.org/plugin.php?id=dsu_paulsign:sign*
-// @include      http://www.repaik.com/plugin.php?id=dsu_paulsign:sign*
-// @include      http://bbs.kafan.cn/thread-*-*-*.html
+// @downloadURL    [url]https://raw.githubusercontent.com/jasonshaw/userscript/master/autoClick1by1.user.js[/url]
+// @updateURL    [url]https://raw.githubusercontent.com/jasonshaw/userscript/master/autoClick1by1.user.js[/url]
+// @note         允许在配置中直接给出dom元素 而替代css3 selector数组，参数直接为函数，返回值即dom元素数组
 // @note         允许是定standby状态，用于要点击的内容依赖页面初始加载后的动态加载才能运行的情况，默认直接运行系列化点击，否则等待动态加载后运行
 // @note         允许自定义网站的点击延迟时间
 // @note         允许自定义网站的是否在系列点击之后关闭网页
@@ -17,45 +22,57 @@
 // @note         支持睿派克、人大论坛自动等自动签到
 // @note         支持卡饭、睿派克自动关闭侧栏
 // @note         支持太平洋汽车本页展开全部内容
+// @grant        GM_openInTab
 // @run-at       document-end
 // @copyright  2014+, jasonshaw
 // ==/UserScript==
 (function(){
 	var autoClose = false,delay = 500,standby = false;
 	var prefs = {
+		'rutracker': {
+			startReg: /^http:\/\/rutracker\.org\/forum\/viewtopic\.php\?t=.*/i,//定义href正则
+			elements: function(){ return document.querySelectorAll('div[class="sp-head folded"]');} ,//不为数组而是函数时，直接提供一个捕获所有点击元素的函数方法
+			delay: 500
+		},//坛友 amf需要增加的，自动展开折叠的帖子，不要用坛友直接删除这个规则
 		'kds': {
 			standby: false,//定义是否，存在等待
 			startReg: /http:\/\/model\.kdslife\.com\/show\/photo\/\d+\.html/i,//定义href正则
 			autoClose: true,//config中dom.allow_scripts_to_close_windows  需要为true, 存在风险，请谨慎使用
 			elements: ['.bigp_nav2 > form > input[value="stop"]'],//所有参数为要点击的按钮的css3 selector
 			delay: 500
-		},
+		},//宽带山美图库阻止自动播放，方便autopager翻页
 		'repaik': {
 			startReg: /http:\/\/www\.repaik\.com\/forum\.php\?mod=viewthread&tid=\d+/i,
 			elements: ['a.btn_s_close']
-		},
+		},//睿派克关闭侧栏
+		'repaik1': {
+			standby: false,//这里的自动签到，就是动态加载，需要等待签到所需的dom元素和js加载和执行完毕，再运行自动化点击实现签到
+			autoClose: true,
+			startReg: /http:\/\/www\.repaik\.com\/plugin\.php\?id=dsu_paulsign:sign/,
+			elements: ['ul.qdsmile > li#ch','table[class="tfm"] input[value="2"]','.tr3 > div:nth-child(2) > a > img']
+		},//睿派克自动签到
+		'repaik2': {
+			startReg: /http:\/\/www\.repaik\.com\/$/,
+			elements: ['a[href$="plugin.php?id=dsu_paulsign:sign"]']
+		},//睿派克自动跳转到签到
 		'pcauto': {
 			startReg: /http:\/\/\w+\.pcauto\.com\.cn\/.+\.html/i,
 			elements: ['div.pageViewGuidedd > a[rel="nofollow"]']
 		},
-		'pinggu': {
-			startReg: /http:\/\/bbs\.pinggu\.org\/plugin\.php\?id=dsu_paulsign:sign/,
-			elements: ['ul.qdsmile > li#fd','table[class="tfm qdtfm"] input[value="2"]','td.qdnewtd3 > a']
-		},
-		'repaik1': {
-			standby: true,//这里的自动签到，就是动态加载，需要等待签到所需的dom元素和js加载和执行完毕，再运行自动化点击实现签到
-			autoClose: true,
-			startReg: /http:\/\/www\.repaik\.com\/plugin\.php\?id=dsu_paulsign:sign/,
-			elements: ['ul.qdsmile > li#ch','table[class="tfm"] input[value="2"]','.tr3 > div:nth-child(2) > a > img']
-		},
 		'kafan': {
-			startReg: /http:\/\/bbs\.kafan\.cn\/thread-\d+-\d+-\d+\.html/,
+			startReg: /http:\/\/bbs\.kafan\.cn\/(thread-\d+-\d+-\d+\.html|forum.php\?mod=viewthread.*)/,
 			elements: ['a.btn_s_close']
+		},//kafan关闭侧栏
+		'kafan1': {
+			startReg: /http:\/\/bbs\.kafan\.cn\/$/,
+			elements: ['a#pper_a']
+		}//kafan每日签到
 		}
 	};
 	function autoClick1by1(){
 		var href = window.location.href,site = null,i = 0;
 		for (var key in prefs) if(prefs[key].startReg.test(href)) {site = key;break;}
+		//alert(site);
 		if(site == null) return;
 		var elements = prefs[site].elements;
 		autoClose = prefs[site].autoClose || autoClose;
@@ -63,11 +80,15 @@
 		standby = prefs[site].standby || standby;
 		setTimeout(function(){
 			try {
-				var elements = prefs[site].elements;
-				while(elements[i]){
-					var obj = document.querySelector(elements[i]);
+				if(elements instanceof Array) var els = prefs[site].elements;
+				else {//function
+					var els = prefs[site].elements();
+				}
+				while(els[i]){
+					var obj = (prefs[site].elements instanceof Array)?document.querySelector(els[i]):els[i];
 					if(obj == null) return;
-					obj.click();
+					if(obj.tagName=="A" && obj.href.indexOf("javascript")<0 && obj.onclick == "undefined") GM_openInTab(obj.href);
+					else obj.click();
 					i++;
 				}
 			} catch(e){alert(e);}
